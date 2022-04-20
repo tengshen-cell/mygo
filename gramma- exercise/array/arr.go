b2c0390a55fd82048a975ed95ea7534e660e2c24
GuangYin Teng <1766707503@qq.com>
2022-04-20 09:48:32 -0400

    array

---
+++ gramma- exercise/array/arr.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func foo(arr [5]int) {
+
+}
+
+func main() {
+	var arr1 [5]int
+	// var arr2 [6]int
+	// var arr3 [5]string
+
+	foo(arr1)
+	// foo(arr2)
+	// foo(arr3)
+
+	var arr = [6]int{1, 2, 3, 4, 5, 6}
+	fmt.Println("数组长度：", len(arr))
+	fmt.Println("数组大小：", unsafe.Sizeof(arr))
+}
